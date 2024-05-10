c9d91812df0da242a86800bb1ce99dd197784457
RealBrandon <brandonhan2068@gmail.com>
2024-05-10 11:43:30 +0100

    Add touchpad_switch.sh

---
+++ touchpad_switch.sh
@@ -0,0 +1,11 @@
+STATE=$1
+
+if [[ "$STATE" == 'on' ]]; then
+    gsettings set org.gnome.desktop.peripherals.touchpad send-events enabled
+    echo 'Touchpad enabled.'
+elif [[ "$STATE" == 'off' ]]; then
+    gsettings set org.gnome.desktop.peripherals.touchpad send-events disabled
+    echo 'Touchpad disabled.'
+else
+    echo 'Invalid input. Please try again.'
+fi
